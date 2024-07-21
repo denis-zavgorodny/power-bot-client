c28c57f465eb8bf391a10766d8333a3dc2eb728c
denys.zavhorodnii <denis.zavgorodny@gmail.com>
2024-07-21 11:17:49 +0300

    add retries

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import os
 import requests
+from requests.adapters import HTTPAdapter
+import logging
+
 from dotenv import dotenv_values
 import time
+from urllib3.util.retry import Retry
+
+logger = logging.getLogger("power-bot-sensor")
+logging.basicConfig(filename="power-bot-sensor.log")
 
 current_file_path = os.path.abspath(__file__)
 current_file_dir = os.path.dirname(current_file_path)
@@ -10,23 +17,49 @@ config = dotenv_values(current_file_dir + "/.env")
 
 
 def main():
-    data = {
-        'timestamp': time.time(),
-    }
+    try:
 
-    headers = {
-        "API-KEY": config.get("API_KEY")
-    }
+        data = {
+            'timestamp': time.time(),
+        }
 
-    response = requests.post(config.get("PING_ENDPOINT"), json=data, headers=headers)
+        headers = {
+            "API-KEY": config.get("API_KEY")
+        }
 
-    if response.status_code == 200:
-        print('Request was successful')
-        print('Response data:', response.json())
-    else:
-        print('Request failed with status code:', response.status_code)
-        print('Response content:', response.text)
+        session = requests_session_with_retries(retries=3, backoff_factor=0.5)
 
+        response = session.post(config.get("PING_ENDPOINT"), json=data, headers=headers)
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            print('Request was successful')
+            print('Response data:', response.json())
+        else:
+            logger.error(f"Request failed with status code: {response.status_code}")
+            logger.error(f"Response content: {response.text}")
+
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Status POST request failed: {e}")
+
+
+def requests_session_with_retries(retries=3, backoff_factor=0.3, status_forcelist=(500, 502, 504)):
+    session = requests.Session()
+
+    retry = Retry(
+        total=retries,
+        read=retries,
+        connect=retries,
+        backoff_factor=backoff_factor,
+        status_forcelist=status_forcelist,
+    )
+
+    adapter = HTTPAdapter(max_retries=retry)
+
+    session.mount("http://", adapter)
+    session.mount("https://", adapter)
+
+    return session
 
 
 if __name__ == '__main__':
